c7a2e756ebf654c426c82c89e1b97f27ec05e830
悦者生存 <1002783067@qq.com>
2023-04-02 19:15:52 +0800

    feat: publish sh

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 /*
  * @Author: 悦者生存 1002783067@qq.com
- * @Date: 2023-03-16 22:39:16
+ * @Date: 2023-03-25 12:06:35
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2023-03-25 17:21:36
- * @FilePath: /my-components/vite.config.ts
+ * @LastEditTime: 2023-03-30 22:21:03
+ * @FilePath: /peacock/packages/peacockui/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import Unocss from './config/unocss'
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const resolvePath = (str: string) => path.resolve(__dirname, str)
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [
     react(), // 支持react
     // 添加UnoCSS插件
@@ -24,8 +24,8 @@ export default defineConfig({
     outDir: 'dist', // 打包后的文件
     lib: {
       entry: resolvePath('packages/index.tsx'), // 配置库入口
-      name: 'PeacockUI', // 库名称
-      fileName: 'peacock-ui', // 文件名
+      name: 'peacockui', // 库名称
+      fileName: 'peacockui', // 文件名
       // 打包格式
       formats: ['es', 'umd', 'iife'] //打包的格式，主要是兼容es module和commonJs规范
     },
@@ -41,4 +41,4 @@ export default defineConfig({
     sourcemap: true, // 输出单独 source文件
     cssCodeSplit: true
   }
-})
+}
