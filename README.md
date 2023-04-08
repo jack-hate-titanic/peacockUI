0409d7bab881b23fbdeac00e10e0f7781f3a9d13
悦者生存 <1002783067@qq.com>
2023-04-08 20:30:50 +0800

    feat: add loading

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 <!--
  * @Author: 悦者生存 1002783067@qq.com
- * @Date: 2023-03-25 10:25:01
+ * @Date: 2023-03-26 23:13:46
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2023-04-04 22:52:13
- * @FilePath: /peacockUI/README.md
+ * @LastEditTime: 2023-04-08 19:54:38
+ * @FilePath: /peacock/packages/peacockui/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
-
 # IPeacock-UI
 
 孔雀 UI 库
--- example/App.tsx
+++ example/App.tsx
@@ -2,13 +2,13 @@
  * @Author: 悦者生存 1002783067@qq.com
  * @Date: 2023-03-16 23:03:35
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2023-04-06 23:14:22
+ * @LastEditTime: 2023-04-08 20:18:43
  * @FilePath: /my-components/example/App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useEffect } from 'react';
-import { Button } from '../dist/peacockui'; // 从打包的库里面引入ui组件
-import Loading from '../packages/Loading';
+import Button from '../dist/Button/index'; // 从打包的库里面引入ui组件
+import Loading from '../dist/Loading/index';
 
 function App() {
   useEffect(() => {
