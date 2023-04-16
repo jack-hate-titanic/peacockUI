85e2c3aff016d8520f0091d4c92368a8267e4aac
悦者生存 <1002783067@qq.com>
2023-04-16 17:36:12 +0800

    feat: update_time

--- example/App.tsx
+++ example/App.tsx
@@ -2,7 +2,7 @@
  * @Author: 悦者生存 1002783067@qq.com
  * @Date: 2023-03-16 23:03:35
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2023-04-08 20:18:43
+ * @LastEditTime: 2023-04-16 17:07:18
  * @FilePath: /my-components/example/App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +13,7 @@ import Loading from '../dist/Loading/index';
 function App() {
   useEffect(() => {
     Loading.show();
-    Loading.show();
+    Loading.hide();
     Loading.show();
   }, []);
 
